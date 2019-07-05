6b1e9e1de5f7e7f2e1dbe814084765aa6afa8331
jianglong <hejianglong321>
2019-07-05 18:07:06 +0800

    数组栈实现及动态扩容

---
+++ src/stack/Stack.java
@@ -0,0 +1,34 @@
+package stack;
+
+public class Stack {
+
+    int size = 5;
+
+    int[] content = new int[size];
+
+    int position = -1;
+
+    public void push(int data) {
+        ++position;
+        if (position == size - 1) {
+            int oldSize = size;
+            size = size * 2;
+            int[] tmpContent = new int[size];
+            for (int i = 0; i < oldSize; i++) {
+                tmpContent[i] = content[i];
+            }
+            content = tmpContent;
+        }
+        content[position] = data;
+    }
+
+    public int pop() {
+        if (position == -1) {
+            return 0;
+        }
+        int val = content[position];
+        content[position] = 0;
+        position--;
+        return val;
+    }
+}
---
+++ src/stack/StackTest.java
@@ -0,0 +1,21 @@
+package stack;
+
+public class StackTest {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
