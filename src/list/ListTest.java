be17b3cdc3070f7453e239d7d828a2d397f8b89b
jianglong <hejianglong321>
2019-07-10 14:27:52 +0800

    二分查找变形-查找第一个匹配的值

--- src/list/ListTest.java
+++ src/list/ListTest.java
@@ -3,7 +3,23 @@ package list;
 public class ListTest {
 
     public static void main(String[] args) {
-        singleLinkedListTest2();
+        ListTest test = new ListTest();
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(4);
+        ListNode listNode3 = new ListNode(3);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+
+        ListNode listNode21 = new ListNode(5);
+        ListNode listNode22 = new ListNode(6);
+        ListNode listNode23 = new ListNode(4);
+        listNode21.next = listNode22;
+        listNode22.next = listNode23;
+        ListNode listNode = test.addTwoNumbers(listNode1, listNode21);
+        while (listNode != null) {
+            System.out.print(listNode.val + " ");
+            listNode = listNode.next;
+        }
     }
 
     private static void singleLinkedListTest2() {
@@ -20,4 +36,82 @@ public class ListTest {
         System.out.println("是否是回文连表: " + LinkedListAlgo.isPalindrome(node1));
     }
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resultNode = null;
+        ListNode pre1 = null;
+        ListNode pre2 = null;
+        int l1length = 0;
+        int l2length = 0;
+        ListNode current = l1;
+        ListNode currentL2 = l2;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = pre1;
+            pre1 = current;
+            current = nextNode;
+            l1length++;
+        }
+        while (currentL2 != null) {
+            ListNode nextNode = currentL2.next;
+            currentL2.next = pre2;
+            pre2 = currentL2;
+            currentL2 = nextNode;
+            l2length++;
+        }
+        if (l1length >= l2length) {
+            current = pre1;
+            currentL2 = pre2;
+            int t = 0; // 进位值
+            while (current != null) {
+                int sum = current.val + t;
+                if (currentL2 != null) {
+                    sum += currentL2.val;
+                    currentL2 = currentL2.next;
+                }
+                if (sum >= 10) {
+                    sum = sum - 10;
+                    t = 1;
+                } else {
+                    t = 0;
+                }
+                current.val = sum;
+                current = current.next;
+            }
+            resultNode = pre1;
+        } else {
+            current = pre1;
+            currentL2 = pre2;
+            int t = 0; // 进位值
+            while (currentL2 != null) {
+                int sum = currentL2.val + t;
+                if (current != null) {
+                    sum += current.val;
+                    current = current.next;
+                }
+                if (sum >= 10) {
+                    sum = sum - 10;
+                    t = 1;
+                } else {
+                    t = 0;
+                }
+                currentL2.val = sum;
+                currentL2 = currentL2.next;
+            }
+            resultNode = pre2;
+        }
+        pre1 = null;
+        current = resultNode;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = pre1;
+            pre1 = current;
+            current = nextNode;
+        }
+        return pre1;
+    }
 }
+class ListNode {
+      int val;
+      ListNode next;
+     ListNode(int x) { val = x; }
+  }
--- src/search/Search.java
+++ src/search/Search.java
@@ -6,8 +6,43 @@ public class Search {
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
         int position = searchPosition(arr, 1);
         System.out.println(position);
+        int[] arr2 = new int[]{1,2,3,4,5,8,8,8,8,9, 10, 11,12,13,14,15,16,17,18};
+        int p2 = searchPositionFirstMatch(arr2, 8);
+        System.out.println(p2);
     }
 
+    /**
+     * 查找第一个匹配的
+     * @param arr
+     * @param target
+     * @return
+     */
+    private static int searchPositionFirstMatch(int[] arr, int target) {
+        int low = 0;
+        int height = arr.length - 1;
+
+        while (low <= height) {
+            int middle = low + ((height - low) >> 1);
+            if (target < arr[middle]) {
+                height = middle - 1;
+            } else if (target > arr[middle]) {
+                low = middle + 1;
+            } else {
+                if (middle == 0 || arr[middle - 1] != target) {
+                    return middle;
+                }
+                height = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 二分查找
+     * @param arr
+     * @param target
+     * @return
+     */
     private static int searchPosition(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
