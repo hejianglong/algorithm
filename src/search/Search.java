b7127372227d9eb7286879d30cd95059bde9bac1
jianglong <hejianglong321>
2019-07-10 14:44:05 +0800

    二分查找变形-查找第最后一个匹配的值

--- src/search/Search.java
+++ src/search/Search.java
@@ -4,11 +4,38 @@ public class Search {
 
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
-        int position = searchPosition(arr, 1);
+        int position = searchPosition(arr, 8);
         System.out.println(position);
         int[] arr2 = new int[]{1,2,3,4,5,8,8,8,8,9, 10, 11,12,13,14,15,16,17,18};
         int p2 = searchPositionFirstMatch(arr2, 8);
         System.out.println(p2);
+        int p3 = searchPositionLastMatch(arr2, 8);
+        System.out.println(p3);
+    }
+
+    /**
+     * 查找最后一个匹配的
+     * @param arr
+     * @param target
+     * @return
+     */
+    private static int searchPositionLastMatch(int[] arr, int target) {
+        int low = 0;
+        int height = arr.length - 1;
+        while (low <= height) {
+            int middle = low + ((height - low) >> 1);
+            if (target > arr[middle]) {
+                low = middle + 1;
+            } else if (target < arr[middle]) {
+                height = middle + 1;
+            } else {
+                if (middle == 0 || arr[middle + 1] != target) {
+                    return middle;
+                }
+                height += 1;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -44,26 +71,16 @@ public class Search {
      * @return
      */
     private static int searchPosition(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (end != start) {
-            int middle = (end - start) / 2 + start;
-            if (target == arr[middle]) {
-                return middle;
-            }
-            if (target == arr[start]) {
-                return start;
-            }
-            if (target == arr[end]) {
-                return end;
-            }
+        int low = 0;
+        int height = arr.length - 1;
+        while (low <= height) {
+            int middle = low + ((height - low) >> 1);
             if (target < arr[middle]) {
-                end = middle;
-                continue;
-            }
-            if (target > arr[middle]) {
-                start = middle;
-                continue;
+                height = middle - 1;
+            } else if (target > arr[middle]) {
+                low = middle + 1;
+            } else {
+                return middle;
             }
         }
         return -1;
