2a34cc85436cbcdebb83bd3b13e5f733601732c7
jianglong <hejianglong321>
2019-07-17 11:27:00 +0800

    图广度优先搜索查找 N 度关系

--- src/heap/Graph.java
+++ src/heap/Graph.java
@@ -1,9 +1,6 @@
 package heap;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
-import static jdk.nashorn.internal.objects.Global.print;
+import java.util.*;
 
 /**
  * @author hejianglong
@@ -14,46 +11,30 @@ public class Graph {
 
     private LinkedList<Integer>[] adj;
 
+    public static void main(String[] args) {
+        Graph g = new Graph(8);
+        g.addEdge(0, 1);
+        g.addEdge(0, 3);
+        g.addEdge(1, 2);
+        g.addEdge(1, 4);
+        g.addEdge(3, 4);
+        g.addEdge(2, 5);
+        g.addEdge(4, 5);
+        g.addEdge(4, 6);
+        g.addEdge(5, 7);
+        g.addEdge(6, 7);
+        g.bfs(0,6);
+        System.out.println();
+        List<Integer> friends = g.relation(0, 5);
+        friends.stream().forEach(s -> System.out.println(s));
+    }
+
     public Graph(int v) {
         this.v = v;
         adj = new LinkedList[v];
         for (int i = 0; i < v; i++) {
             adj[i] = new LinkedList<>();
         }
-    }
-
-    public void bfs(int s, int t) {
-        if (s == t) return;
-        boolean[] visited = new boolean[v];
-        visited[s] = true;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(s);
-        int[] prev = new int[v];
-        for (int i = 0; i < v; i++) {
-            prev[i] = - 1;
-        }
-        while (queue.size() != 0) {
-            int w = queue.poll();
-            for (int i = 0; i < adj[w].size(); i++) {
-                int q = adj[w].get(i);
-                if (!visited[q]) {
-                    prev[q] = w;
-                    if (q == t) {
-                        print(prev, s, t);
-                        return;
-                    }
-                    visited[q] = true;
-                    queue.add(q);
-                }
-            }
-        }
-    }
-
-    private void print(int[] prev, int s, int t) {
-        if (prev[t] != -1 && t != s) {
-            print(prev, s, prev[t]);
-        }
-        System.out.print(t + " ");
     }
 
     /**
@@ -64,5 +45,68 @@ public class Graph {
     public void addEdge(int s, int t) {
         adj[s].add(t);
         adj[t].add(s);
+    }
+
+    public void bfs(int s, int t) {
+        if (s == t) {
+            return;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        boolean[] visited = new boolean[v];
+        int[] prev = new int[v];
+        for (int i = 0; i < v; i++) {
+            prev[i] = -1;
+        }
+        visited[s] = true;
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < adj[w].size(); i++) {
+                int q = adj[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                    queue.add(q);
+                    visited[q] = true;
+                }
+            }
+        }
+    }
+
+    public List relation(int s, int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        boolean[] visited = new boolean[v];
+        List<Integer> friends = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(s, 0);
+        visited[s] = true;
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < adj[w].size(); i++) {
+                Integer q = adj[w].get(i);
+                Integer num = map.get(w) + 1;
+                map.put(q,num);
+                if (num > n) {
+                    continue;
+                }
+                if (!visited[q]) {
+                    friends.add(q);
+                    visited[q] = true;
+                    queue.add(q);
+                }
+            }
+        }
+        return friends;
+    }
+
+    private void print(int[] prev, int s, int t) {
+        if (prev[t] != -1 && s != t) {
+            print(prev, s, prev[t]);
+        }
+        System.out.print(t + " ");
     }
 }
